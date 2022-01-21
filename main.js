143f04d87b2a7690d0223d2c6c504ec4038ed7c2
kim-min-ju449 <77184157+kim-min-ju449@users.noreply.github.com>
2022-01-21 23:29:30 +0900

    JS commit

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ const inputText = document.querySelector(".input-text");
 const addButton = document.querySelector(".add-button");
 const list = document.querySelector(".list");
 
+const likeButtons = document.querySelectorAll(".like");
+likeButtons.forEach((like)=>{
+    like.addEventListener("click",()=>{
+        console.log("clicked")
+    })
+})
+
 addButton.addEventListener("click", function() {
 
 
@@ -15,12 +22,20 @@ addButton.addEventListener("click", function() {
 
     //item
     const item = document.createElement("span");
+    item.classList.add("item");
     item.innerText = inputText.value;
 
     //manage
     const check = document.createElement("span");
     const checkIcon = document.createElement("i");
     const clearIcon = document.createElement("i");
+    checkIcon.classList.add("material-icons","check");
+    checkIcon.classList.add("material-icons","clear");
+    checkIcon.innerText="check"
+    clearIcon.innerText="clear"
+    manage.classList.add("manage")
+    manage.appendChild(checkIcon)
+    manage.appendChild(clearIcon)
     
     const li = document.createElement("li");
     li.innerText = inputText.value;
