b20ebe78e494ba83c8f12abb2b6bceabc49eed8c
kim-min-ju449 <77184157+kim-min-ju449@users.noreply.github.com>
2022-01-24 17:48:52 +0900

    완성

--- main.js
+++ main.js
@@ -4,11 +4,38 @@ const scoreDisplay = document.querySelector("#score")
 const timeDisplay = document.querySelector("#time")
 const messageDisplay = document.querySelector("#message")
 
+const GAME_TIME = 10;
+
+const API_URL = "https://random-word-api.herokuapp.com/word?number=100"
+
 let words = ["banana", "key","car","javascript","scalper"];
 let score = 0;
+let time = 0;
+let timeInterval;
+let isPlaying = false;
+let isReady = false;
 
+init()
+
+//function init(){
+//    const res = fetch(API_URL).then(res=> res.json()).then((data)=>words = data);
+//    console.log(res)
+
+//}
+
+async function init(){
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    words = data.filter(item=>{
+        return item.length< 7
+        isReady = true;
+    })
+    console.log(words)
+}
 
 const randomIndex = Math.floor(Math.random()*5);
+
+time=GAME_TIME;
 
 
 wordInput.addEventListener("input",e=>{
@@ -20,12 +47,37 @@ wordInput.addEventListener("input",e=>{
     }
    
 })
+function gameover(){
+    console.log("game over")
+    isPlaying = false;
+    clearInterval(timeInterval)
+    timeInterval = null;
+    messageDisplay.innerText = "GAME OVER!!"
+    score = 0;
+}
+
+function countDown(){
+    console.log(time)
+    time = time -1;
+    timeDisplay.innerText = time;
+    if(time ===0){
+        gameover();
+    }
+}
 
 function setNewWord(){
+    time = GAME_TIME;
     wordInput.value = "";
     messageDisplay.innerText = "Now Playing!!"
     const randomIndex = Math.floor(Math.random()*words.length);
     currentWord.innerText = words[randomIndex]
+
+    if(!isPlaying){
+        timeInterval = setInterval(countDown, 1000)
+        isPlaying = true;
+    }
+
+    timeInterval = setInterval(countDown, 1000)
 }
 
 function addScore(){
